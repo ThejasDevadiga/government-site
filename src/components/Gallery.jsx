4301e8ed0b3ec97502fbc9b5e15603cae09b1729
Thejas Devadiga <96642748+ThejasDevadiga@users.noreply.github.com>
2024-06-14 22:52:04 +0530

    Update Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -38,7 +38,7 @@ const MyGallery = () => {
         </div>
         <div className="grid grid-cols-1 items-center sm:grid-cols-2 md:grid-cols-4 gap-5 p-12">
           {images.map((image, index) =>
-          const data=image.photo
+      { const data=image.photo
           const uint8Array = new Uint8Array(data.img.data);
 
          
@@ -52,7 +52,7 @@ const MyGallery = () => {
                   className="w-full h-auto object-cover"
                 />
             
-          ))}
+          )})}
         </div>
       </Gallery>
     </>
